4c0ce5a29eb5e1de1082e694a3ab624c9e8cc500
SemSlipchenko <semslipchenko@gmail.com>
2022-10-07 22:08:24 +0300

    Update trending.js

--- src/js/trending.js
+++ src/js/trending.js
@@ -2,8 +2,7 @@ const apiKey = 'api_key=e32c2b640d0c14cb8349bc85f9ee8b0e'
 const trendingUrl = 'https://api.themoviedb.org/3/trending/movie/week?'
 
 async function trending() { 
-    return await fetch(`${trendingUrl}${apiKey}`).then(response => response.json())
-
+    return await fetch(`${trendingUrl}${apiKey}`).then(response => console.log(response.json()))
 }
 
 trending()

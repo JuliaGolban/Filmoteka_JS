2faf6b893db97ff846ede516a5f2d7ff65fd2657
Roman Kashyrin <103502911+Royale16@users.noreply.github.com>
2022-10-08 13:18:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import * as apiCommon from './js/api-common.js';
 
 import onOpenModalTeam from './js/modalTeamForm';
 import onscroll from './js/scrollTo';
-import localeStorage from './js/localeStorage';
+import * as localeStorage from './js/localeStorage';

ec500a999fa19ca501f722d8d160dfbf896efe7c
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2022-10-09 22:20:10 +0300

    Відкриття модалки по кліку

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import './js/modalTeamForm';
 import './js/scrollTo';
 import * as localeStorage from './js/localeStorage';
 import * as lsCommon from './js/lsCommon';
+import * as modal from './js/modal';

df789b085dcb76bb8186336da360e322ce5d800c
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-10-09 22:12:29 +0300

    apiGenres

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './js/trending';
 import './js/modalTeamForm';
 import './js/scrollTo';
 import * as localeStorage from './js/localeStorage';
+
 import * as apiGenres from './js/api-genres.js';

2df965fca0c4a4fc67d2908e93f13e069f295838
Heorgii <rushchakheorgii@gmail.com>
2022-10-11 13:08:04 +0300

    switcher js

---
+++ src/js/switcher.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+// const bgSwitcherBtn = document.querySelector('');
+
+bgSwitcherBtn.addEventListener('click', () =>{
+    bgSwitcherBtn.classList.toggle('switcher-btn');
+});
+
+
+

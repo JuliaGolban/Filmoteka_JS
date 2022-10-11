4641f63d558f73639a79b3ce55679a768ed4f36f
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-11 12:38:14 +0300

    added closing modal from backdrop

--- src/js/modalTeamForm.js
+++ src/js/modalTeamForm.js
@@ -4,8 +4,15 @@ const refs = getRefs();
 
 refs.openModalTeam.addEventListener('click', onOpenModalTeam);
 refs.closeModalTeam.addEventListener('click', onOpenModalTeam);
+refs.modalTeam.addEventListener('click', onOpenModalTeam);
 
 function onOpenModalTeam() {
   document.body.classList.toggle('modal-team-is-open');
   refs.modalTeam.classList.toggle('is-hidden');
 }
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    refs.modalTeam.classList.add('is-hidden');
+  }
+});

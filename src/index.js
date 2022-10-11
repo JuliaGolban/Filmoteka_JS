ecbf120ab82164d375cdac08d176b0aa07778167
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-11 22:32:27 +0300

    update main js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './js/trending';
-import './js/headerSearch';
 import './js/api-common';
+import './js/trending';
+import './js/api-genres.js';
+import './js/headerSearch';
 import './js/modal';
 import './js/modalTeamForm';
 import './js/scrollTo';

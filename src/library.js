96a3a066f311bb734635684536b25b9e57e97824
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-12 09:28:09 +0300

    deleted conflicts

--- src/library.js
+++ src/library.js
@@ -1,13 +1,9 @@
 import './js/localeStorage';
 import './js/libraryButtonSwitch';
-<<<<<<< HEAD
-import * as burger from './js/burger-menu';
-=======
 import './js/modal';
 import './js/modalTeamForm';
 import './js/scrollTo';
-// import './js/burger-menu';
+import './js/burger-menu';
 import './js/popup';
 import './js/firebase';
 // import './js/switcher';
->>>>>>> main

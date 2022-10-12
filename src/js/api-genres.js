0911d46bcf87ba991e64745ae7f51b3fb8cf6e19
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-12 18:29:44 +0300

    refactor common fn localStorage

--- src/js/api-genres.js
+++ src/js/api-genres.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import LS from './localeStorage';
+import { getFromStorage, saveToStorage } from './localeCommon';
 
 const API_KEYS = 'e32c2b640d0c14cb8349bc85f9ee8b0e';
 const BASE_URL = 'https://api.themoviedb.org/3/genre/movie/list';
@@ -7,61 +7,53 @@ const BASE_URL = 'https://api.themoviedb.org/3/genre/movie/list';
 // Запит на сервер
 
 async function getGenres() {
-    
-    try {
-      const response = await axios.get(BASE_URL, {
-        params: {
-            api_key: API_KEYS,
-        }
-      })
-      const dataGenres = response.data.genres;
-     
-       return dataGenres;
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        api_key: API_KEYS,
+      },
+    });
+    const dataGenres = response.data.genres;
 
-    } catch (error) {
-      console.error(error);
-    }
+    return dataGenres;
+  } catch (error) {
+    console.error(error);
   }
+}
 
 //   Запис в LocalStorege масиву обєктів з жанрами.
 
-function saveGenresLocalStorege () {
-    getGenres().then((response) => {
-        LS.save('genres', response) 
-    })
+function saveGenresLocalStorege() {
+  getGenres().then(response => {
+    saveToStorage('genres', response);
+  });
 }
 
-saveGenresLocalStorege ()
+saveGenresLocalStorege();
 
 // Отримати жанри з LocalStorege.
 // Аргументом в функцію getGenresLocalStorege(genreIds) треба передати масив з id номерами.
 // Функція повертає рядок з назвами жанрів (приклад - 'Action, Western, Comedy') з лімітом по довжині 4 жанри.
 
-export function getGenresLocalStorege (genreIds) {
-  const parsedJsonGenres = LS.load('genres')
-  if(parsedJsonGenres === undefined) {
-    return 'Not-found'
+export function getGenresLocalStorege(genreIds) {
+  const parsedJsonGenres = getFromStorage('genres');
+  if (parsedJsonGenres === undefined) {
+    return 'Not-found';
   }
 
   let nameGenres = [];
 
-  for (let id of  genreIds) {
+  for (let id of genreIds) {
     parsedJsonGenres.forEach(genre => {
-        if(id === genre.id) {
-            nameGenres.push(genre.name)
-        }
-    })
+      if (id === genre.id) {
+        nameGenres.push(genre.name);
+      }
+    });
   }
   const nameGenresSlice = nameGenres.slice(0, 4);
-  if(nameGenres.length > 4){
+  if (nameGenres.length > 4) {
     nameGenresSlice.push('etc.');
   }
 
   return nameGenresSlice.join(', ');
 }
-
-
-
-
-
-

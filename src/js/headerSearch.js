1cd9fffdf3bc5e25f73f4819410b5035ff86f13e
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-12 21:15:11 +0300

    Update headerSearch.js

--- src/js/headerSearch.js
+++ src/js/headerSearch.js
@@ -24,7 +24,7 @@ function onSubmitForm(event) {
 function search() {
   let currentPage = 1;
 
-  partUrl = localStorage.getItem('paramsPart');
+  let partUrl = localStorage.getItem('paramsPart');
 
   getResponse(currentPage, partUrl);
 }

ffef7a32f4a90a18c45eeeaaa06501903558964f
Heorgii <rushchakheorgii@gmail.com>
2022-10-14 21:07:43 +0300

    some

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -29,7 +29,6 @@ const firebaseApp = initializeApp({
     appId: "1:631345881209:web:0b819b68909f7ff5c634a8",
     measurementId: "G-J0W5VCW7RT"
 });
-
 const auth = getAuth(firebaseApp);
 
 const loginEmailPassword = async (e) => {
@@ -41,7 +40,7 @@ const loginEmailPassword = async (e) => {
     }
     refs.successEnter.classList.remove('is-hidden');
     refs.formSuccessMsg.classList.add('is-hidden');
-    refs.formTitle.classList.remove('modal-title');
+    refs.formTitle.classList.remove('modal-login-title');
     refs.formTitle.classList.add('is-hidden');
     refs.btnLogout.classList.remove('is-hidden');
 
@@ -67,9 +66,9 @@ const createAccout = async (e) => {
     }
     refs.successEnter.classList.remove('is-hidden');
     refs.formSuccessMsg.classList.add('is-hidden');
-    refs.formTitle.classList.remove('modal-title');
+    refs.formTitle.classList.remove('modal-login-title');
     refs.formTitle.classList.add('is-hidden');
-    refs.btnLogout.classList.remove('is-hidden');
+    // refs.btnLogout.classList.remove('is-hidden');
     try {
         await createUserWithEmailAndPassword(auth, email, password);
     }
@@ -99,7 +98,13 @@ const logout = async () => {
     await signOut(auth);
     window.location.reload();
 }
+
+// const logOutFromForm = async () =>{
+//     while
+// }
 refs.btnLogin.addEventListener("click", loginEmailPassword);
 refs.btnSignup.addEventListener("click", createAccout);
 refs.btnLogout.addEventListener('click', logout);
+// refs.openModalUserBtn.addEventListener('click', logOutFromForm);
+
 monitorAuthState();

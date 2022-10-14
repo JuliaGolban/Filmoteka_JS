6d60af4064683cd347ae7bf06cd9e42900a13a3d
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2022-10-14 18:09:22 +0300

    При перемиканні теми змінює іконку , виправляє колір в бібліотеці. Виправляє положення модального вікна в мобільній версіі

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,9 @@
 function theme(){
     const toggleTheme = document.querySelector('#toggle-theme-btn');
-    // const sun = document.querySelector()
+    const sun = document.querySelector('.sun');
+    const moon =document.querySelector('.moon');
+   
+
     if(localStorage.getItem('theme') !== null){
         document.documentElement.setAttribute('data-theme', 'dark')
     }
@@ -8,10 +11,15 @@ function theme(){
 
         if(document.documentElement.hasAttribute('data-theme')){
             document.documentElement.removeAttribute('data-theme')
+            moon.classList.toggle('is-hidden')
+            sun.classList.toggle('is-hidden')
+
             localStorage.removeItem('theme')
         }
         else{
             document.documentElement.setAttribute('data-theme', 'dark')
+            sun.classList.toggle('is-hidden')
+            moon.classList.toggle('is-hidden')
             localStorage.setItem('theme', 'dark')
         }
  

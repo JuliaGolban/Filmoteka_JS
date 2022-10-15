1fc5df6d299eaf0d08d6ae06db2c366efc2a6bb8
Dima <57713733+Dima1409@users.noreply.github.com>
2022-10-15 10:53:28 +0300

    auto close menu genres else scroll body > 200

--- src/js/sortGenres.js
+++ src/js/sortGenres.js
@@ -11,18 +11,30 @@ function onClickMenu() {
   refs.secondLine.classList.toggle('genres-nav-button__line--2');
   refs.thirdLine.classList.toggle('genres-nav-button__line--3');
   refs.menu.classList.toggle('mobile-menu--open');
-
   renderGenres(getFromStorageGenre('genres'));
-  if((refs.menu.classList.contains('mobile-menu--open'))) {
-  
-    setTimeout(()=>{
-      refs.menu.classList.remove('mobile-menu--open');
-      refs.firstLine.classList.toggle('genres-nav-button__line--1');
-      refs.secondLine.classList.toggle('genres-nav-button__line--2');
-      refs.thirdLine.classList.toggle('genres-nav-button__line--3');
-    },10000)
-  }
 }
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        refs.menu.classList.remove('mobile-menu--open');
+        refs.firstLine.classList.remove('genres-nav-button__line--1');
+        refs.secondLine.classList.remove('genres-nav-button__line--2');
+        refs.thirdLine.classList.remove('genres-nav-button__line--3');
+    }
+
+}
+
+
+// if(!refs.scrollToTop.classList.contains('is-hidden')) {
+//   setTimeout(()=>{
+//     refs.menu.classList.remove('mobile-menu--open');
+//     refs.firstLine.classList.toggle('genres-nav-button__line--1');
+//     refs.secondLine.classList.toggle('genres-nav-button__line--2');
+//     refs.thirdLine.classList.toggle('genres-nav-button__line--3');
+//   },5000)
+// }
 
 function renderGenres(results) {
   refs.menuList.innerHTML = '';

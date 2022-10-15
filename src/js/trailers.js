b3419b9352ee1471a7139c441cad0de71a6fda89
OlehHritsenko <grolan12345@gmail.com>
2022-10-15 19:52:49 +0300

    adaptive of youtube player

--- src/js/trailers.js
+++ src/js/trailers.js
@@ -28,7 +28,7 @@ function createTrailerLink(elementRef) {
     
     const instance = basicLightbox.create(`
     <div class="modal">
-    <iframe width="560" height="315" src='https://www.youtube.com/embed/${id}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="100%" height="315" src='https://www.youtube.com/embed/${id}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   `);
           instance.show();

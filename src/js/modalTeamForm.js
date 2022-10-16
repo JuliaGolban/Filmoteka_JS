968023459ff89c9ef91e5583e4006a5ed45df972
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-16 17:36:24 +0300

    added description member's fn

--- src/js/modalTeamForm.js
+++ src/js/modalTeamForm.js
@@ -4,18 +4,10 @@ const refs = getRefs();
 
 refs.openModalTeam.addEventListener('click', onOpenModalTeam);
 refs.closeModalTeam.addEventListener('click', onOpenModalTeam);
-refs.modalTeam.addEventListener('click', onBackdropModalTeam);
 
 function onOpenModalTeam() {
   document.body.classList.toggle('modal-team-is-open');
   refs.modalTeam.classList.toggle('is-hidden');
-
-  document
-    .querySelector('.modal-team__image')
-    .addEventListener('click', onCardMember);
-  document
-    .querySelector('.modal-team__feature')
-    .addEventListener('click', onCardMember);
 }
 
 document.addEventListener('keydown', function (e) {
@@ -23,13 +15,3 @@ document.addEventListener('keydown', function (e) {
     refs.modalTeam.classList.add('is-hidden');
   }
 });
-
-function onBackdropModalTeam(e) {
-  if (e.currentTarget === e.target) {
-    refs.modalTeam.classList.add('is-hidden');
-  }
-}
-
-function onCardMember() {
-  document.querySelector('.modal-team__feature').classList.toggle('show');
-}

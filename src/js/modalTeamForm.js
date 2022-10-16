65d0ad0807349b9429218f16bfbb30e84c3ee882
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-16 17:04:01 +0300

    added one part

--- src/js/modalTeamForm.js
+++ src/js/modalTeamForm.js
@@ -9,6 +9,13 @@ refs.modalTeam.addEventListener('click', onBackdropModalTeam);
 function onOpenModalTeam() {
   document.body.classList.toggle('modal-team-is-open');
   refs.modalTeam.classList.toggle('is-hidden');
+
+  document
+    .querySelector('.modal-team__image')
+    .addEventListener('click', onCardMember);
+  document
+    .querySelector('.modal-team__feature')
+    .addEventListener('click', onCardMember);
 }
 
 document.addEventListener('keydown', function (e) {
@@ -21,4 +28,8 @@ function onBackdropModalTeam(e) {
   if (e.currentTarget === e.target) {
     refs.modalTeam.classList.add('is-hidden');
   }
+}
+
+function onCardMember() {
+  document.querySelector('.modal-team__feature').classList.toggle('show');
 }

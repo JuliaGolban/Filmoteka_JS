55f65a6388deafb7ecc02e7e481b2222ba647cfb
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-16 08:56:07 +0300

    fix footer absolute

--- src/js/fixFooter.js
+++ src/js/fixFooter.js
@@ -1,6 +1,7 @@
 import getRefs from './getRefs';
 const refs = getRefs();
+console.log('refs', refs);
 
-if (refs.galleryList.childElementCount > 9) {
-  refs.footer.classList.remove('fixed');
+if (refs.galleryList.childElementCount < 9) {
+  refs.footer.classList.add('fixed');
 }
